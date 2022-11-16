e316521904a92025c55903e92c05849f51147e03
Max2288 <kennySRU@ya.ru>
2022-11-16 18:49:22 +0300

    Добавлены все тесты

--- test_main.py
+++ test_main.py
@@ -29,3 +29,21 @@ test_for_radius = [(1), (5), (7.0), (10000.1)]
 def test_radius(radius):
     """Test for radius."""
     assert Ball(radius).radius == radius
+
+
+test_for_uniform_motion = [(5, 5, 5, 286.4789), (6, 1, 1000, 189.29659), (1000, 0.1, 1000, 5.72958)]
+
+
+@pytest.mark.parametrize('radius, speed, time, expectation', test_for_uniform_motion)
+def test_uniform(radius, speed, time, expectation):
+    """Test for uniform motion."""
+    assert Ball(radius).motion(speed, time) == expectation
+
+
+test_for_uniformly_accelerated_motion = [(5, 10, 10, 10, 35.49354), (2, 0, 1, -10, 216.76055), (1, 1, 1, 1, 85.94367)]
+
+
+@pytest.mark.parametrize('radius, speed, time, acceleration, expectation', test_for_uniformly_accelerated_motion)
+def test_uniformly_accelerated(radius, speed, time, acceleration, expectation):
+    """Test for uniformly accelerated motion."""
+    assert Ball(radius).motion(speed, time, acceleration) == expectation
